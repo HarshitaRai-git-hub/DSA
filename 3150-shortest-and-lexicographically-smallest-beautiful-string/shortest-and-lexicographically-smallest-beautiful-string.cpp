ed38227e46dce1306c9a7e08eaa1396c5867bfaf
HarshitaRai-git-hub <raiharshita1616@gmail.com>
2026-08-26 19:35:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (25.19%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -6,22 +6,23 @@ public:
         int mini=INT_MAX;
         int cnt=0;
         while(j<s.size()){
-            // while(i<s.size()&&s[i]!='1')i++;
-            // if(j<i)j=i;
+            while(i<s.size()&&s[i]!='1')i++;
+            if(j<i)j=i;
             if(s[j]=='1')cnt++;
-            while(cnt == k){
-                while(s[i] == '0') i++;
-                mini = min(mini, j - i + 1);
-                if(s[i] == '1') cnt--;
-                i++;
-            }
-            j++;
-            // if(cnt<k)j++;
-            // else if(cnt==k){
-            //     mini=min(mini,j-i+1);
-            //     if (s[i] == '1')cnt--;
+            // while(cnt == k){
+            //     while(s[i] == '0') i++;
+            //     mini = min(mini, j - i + 1);
+            //     if(s[i] == '1') cnt--;
             //     i++;
             // }
+            // j++;
+            if(cnt<k)j++;
+            else if(cnt==k){
+                mini=min(mini,j-i+1);
+                if (s[i] == '1')cnt--;
+                i++;
+                j++;
+            }
         }
         vector<string> s1;
         i=0,j=0;
