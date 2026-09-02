21aa3512c45d3131832f0dea2c850acd6f64f7aa
HarshitaRai-git-hub <raiharshita1616@gmail.com>
2026-09-02 17:55:53 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (36.43%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // int n=nums1.size();
+        // int parity=((nums1[0]-nums1[1])%2);
+        // for(int i=1;i<n-1;i++){
+        //     if((nums1[i]-nums1[i+1])!=parity)return false;
+        // }
+        // if(nums1[n-1]%2!=parity)return false;
+        return true;
+    }
+};
